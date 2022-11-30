916ccc60aff623a27f2b74a01a4d20b84240bf21
Rustam <pkheshkhov0707@mail.ru>
2022-11-30 17:41:47 +0300

    третья задача01

--- задача 3/Program.cs
+++ задача 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+if (number > 7 || number <= 0)
+{
+  Console.WriteLine("такого дня нет");  
+}
+if (number == 6 || number == 7 )
+{
+ Console.WriteLine("да");   
+} else {
+    Console.WriteLine("нет");
+}
